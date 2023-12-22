9323b7168c464b30b423eda16ad1f3fc99013c7a
reydenshi0 <154588599+reydenshi0@users.noreply.github.com>
2023-12-22 21:41:41 +0300

    exam3

---
+++ Exam1/Exam1/Exam0.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Exam1
+{
+    internal class Exam0
+    { 
+        public static void Main(string[] args)
+        {
+           
+            Random rand = new Random();
+            int i = rand.Next(0, 11);
+            int count = 1;
+            Console.WriteLine("Компьютер загадал число от 0 до 10.Угадайте его");
+            Console.WriteLine("Введите первое число");
+            int k = Convert.ToInt32(Console.ReadLine());
+            while (count <= 3)
+              {
+                if (i == k)
+                {
+                    Console.WriteLine("Компьютер загадал число " + k + "!");
+                    break;
+                }
+                else
+                {
+                    count++;
+                    if (count == 4)
+                    {
+                        Console.WriteLine("Вы не угадали!. Загаданное число было"  + i + "!" );
+                        break;
+                    }
+                    Console.WriteLine("Нет,это число " + k + "! Попытка № " + count +":");
+                    k = Convert.ToInt32(Console.ReadLine());
+                }
+
+              }
+            Console.ReadLine();
+
+        }
+    
+    }
+}
